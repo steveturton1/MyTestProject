ba541bf9afbb7b8f41afa5646d708bce52bca2e3
steveturton1 <steve.turton@visualretailing.com>
2014-12-30 10:43:26 +0000

    shows a big image when selected - roughly.

--- services/MyData.py
+++ services/MyData.py
@@ -33,154 +33,154 @@ def get_toolbar():
     shirt['id'] = 'white'
     shirt['display_name'] = 'White'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-white-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'black'
     shirt['display_name'] = 'Black'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-black-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-black-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-black-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'apple'
     shirt['display_name'] = 'Apple'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-apple-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-apple-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'caribbean-blue'
     shirt['display_name'] = 'Caribbean Blue'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-caribbean-blue-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-caribbean-blue-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'charcoal'
     shirt['display_name'] = 'Charcoal'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-charcoal-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-charcoal-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'chocolate'
     shirt['display_name'] = 'Chocolate'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-chocolate-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-chocolate-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'city-green'
     shirt['display_name'] = 'City Green'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-city-green-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-city-green-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'heather-blue'
     shirt['display_name'] = 'Heather Blue'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-heather-blue-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-heather-blue-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'heather-dark-grey'
     shirt['display_name'] = 'Heather Dark Grey'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-heather-dark-grey-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-heather-dark-grey-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'heather-green'
     shirt['display_name'] = 'Heather Green'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-heather-green-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-heather-green-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'heather-grey'
     shirt['display_name'] = 'Heather Grey'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-heather-grey-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-heather-grey-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'heather-purple'
     shirt['display_name'] = 'Heather Purple'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-heather-purple-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-heather-purple-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'independence-red'
     shirt['display_name'] = 'Independence Red'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-independence-red-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-independence-red-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'kelly-green'
     shirt['display_name'] = 'Kelly Green'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-kelly-green-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-kelly-green-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'light-blue'
     shirt['display_name'] = 'Light Blue'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-light-blue-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-light-blue-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'navy'
     shirt['display_name'] = 'Navy'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-navy-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-navy-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'orange'
     shirt['display_name'] = 'Orange'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-orange-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-orange-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'purple'
     shirt['display_name'] = 'Purple'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-purple-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-purple-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'red'
     shirt['display_name'] = 'Red'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-red-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-red-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'royal-blue'
     shirt['display_name'] = 'Royal Blue'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-royal-blue-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-royal-blue-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'smoke'
     shirt['display_name'] = 'Smoke'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-smoke-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-smoke-medium.png')
     shirts.append(shirt)
 
     shirt = {}
     shirt['id'] = 'spring-yellow'
     shirt['display_name'] = 'Spring Yellow'
     shirt['img_small'] = url_for('static', filename='images/shirts/anvil-spring-yellow-small.jpg')
-    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-white-medium.jpg')
+    shirt['img_medium'] = url_for('static', filename='images/shirts/anvil-spring-yellow-medium.png')
     shirts.append(shirt)
 
     toolbar['shirts'] = {}
--- static/js/main-canvas.js
+++ static/js/main-canvas.js
@@ -6,20 +6,12 @@ MainCanvas.prototype = {};
 MainCanvas.prototype.constructor = MainCanvas;
 
 function MainCanvas() {
+	// maybe wrap this in a vars object for tidyness
+
     this.canvas = document.getElementById("mc-canvas"),
 	this.context = this.canvas.getContext("2d"),
-	this.image = new Image(),
-	this.imageObject = {x : 10,	y : 10, width : 300, height : 200 },
-	this.clickObject = false,
-	this.selectObjectBorderPadding = 2,
-	this.selectObjectGrabWidth = 12,		// Keep an even number
-	this.selectObjectBorder = {},
-	this.selectObjectGrabHandle = {},
-	this.dragging = false,
-	this.resizing = false,
-	this.dragLoc,
 	this.canvasBackgroundImageData,
-	this.x = 0;
+	this.garmentImage = new Image();
 
     this.canvas.onmousedown = onmousedown;
 }
@@ -27,21 +19,38 @@ function MainCanvas() {
 
 MainCanvas.prototype.testmouseover=function(element) {
 
+	// do this better - I keep finding the selected image.  Just do it once.
+
 	var img = $(element).find("img").attr("data-img-medium");
 
-	var x = $(element).parent().parent().find("li").find("img").removeClass("selected_img");
+	// Get the display name and display it.
+	$("#mcts-colour").text($(element).find("img").attr("data-displayname"));
+
+	// Deselect any selected images (should only be 1)
+	$(element).parent().parent().find("li").find("img").removeClass("selected-img");
+
+	// Select the clicked image
 	$(element).find("img").addClass("selected-img");
 
-
-
+	this.initialise_canvas(img);
 };
 
 
-MainCanvas.prototype.initialise_canvas=function() {
-	this.context.translate(0.5, 0.5);	// so all lines straddle the pixels and aren't blurred - http://www.mobtowers.com/html5-canvas-crisp-lines-every-time/
+MainCanvas.prototype.initialise_canvas=function(img) {
+	//this.context.translate(0.5, 0.5);	// so all lines straddle the pixels and aren't blurred - http://www.mobtowers.com/html5-canvas-crisp-lines-every-time/
     this.renderGrid('whitesmoke', 10, 10);
-    this.saveCanvasBackground();
-    this.renderAll();
+
+	var _this = this;
+
+	this.garmentImage.onload = function(e) {
+		_this.context.drawImage(_this.garmentImage, 0, 0);
+
+		_this.saveCanvasBackground();
+    	_this.renderAll();
+	};
+
+	this.garmentImage.src = img;
+
 };
 
 MainCanvas.prototype.renderGrid=function(color, stepx, stepy) {
@@ -76,266 +85,9 @@ MainCanvas.prototype.restoreCanvasBackground=function() {
 };
 
 MainCanvas.prototype.renderAll=function() {
-		this.restoreCanvasBackground();
-		//this.renderObject();
-
-		//if (clickObject == true) {
-		//	//context.fillRect(10, 10, 10, 10);
-		//	selectObject();
-		//}
+	this.restoreCanvasBackground();
 };
 
 onmousedown=function(e){
-    var x = 1;
-		//if (resizing == true) {
-		//	renderAll();
-		//};
-
-		//dragging = false;
-		//resizing = false;
-		//canvas.style.cursor = "default";
+    //var x = 1;
 };
-
-    /*
-	// maybe wrap this in a vars object for tidyness
-
-	// On resizing image - set transparency.
-
-    var xxx = document.getElementById("main-container");
-	var canvas = document.getElementById("mc-canvas");
-	var context = canvas.getContext("2d");
-	var image = new Image();
-	var imageObject = {x : 10,	y : 10, width : 300, height : 200 };
-	var clickObject = false;
-	var selectObjectBorderPadding = 2;
-	var selectObjectGrabWidth = 12;		// Keep an even number
-	var selectObjectBorder = {};
-	var selectObjectGrabHandle = {};
-	var dragging = false;
-	var resizing = false;
-	var dragLoc;
-	var canvasBackgroundImageData;
-
-	canvas.onmouseup = function (e) {
-
-		if (resizing == true) {
-			renderAll();
-		};
-
-		dragging = false;
-		resizing = false;
-		canvas.style.cursor = "default";
-	}
-
-	canvas.onmousedown = function (e) {
-		e.preventDefault();
-
-		if (canvas.style.cursor == "nw-resize") {
-			dragLoc = windowToCanvas(e.clientX, e.clientY);
-			resizing = true;
-			return;
-		}
-
-		var loc = windowToCanvas(e.clientX, e.clientY);
-
-		context.beginPath();
-		context.rect(imageObject.x, imageObject.y, imageObject.width, imageObject.height);
-
-		if (context.isPointInPath(loc.x, loc.y)) {
-			clickObject = true;
-			dragging = true;
-			dragLoc = windowToCanvas(e.clientX, e.clientY);
-			//canvas.style.cursor = "move";
-		}
-		else
-		{
-			clickObject = false;
-			dragging = false;
-			//canvas.style.cursor = "default";
-		}
-
-		renderAll();
-	}
-
-	canvas.onmousemove = function (e) {
-		//prevent default thing - if dragging
-
-		var loc = windowToCanvas(e.clientX, e.clientY);
-
-		if (dragging == true) {
-			imageObject.x += loc.x - dragLoc.x;
-			imageObject.y += loc.y - dragLoc.y;
-
-			dragLoc.x = loc.x;
-			dragLoc.y = loc.y;
-			renderAll();
-		}
-		else
-		if (resizing == true) {
-
-			var normalResize = true;
-
-			if (normalResize == false) {
-				imageObject.width += loc.x - dragLoc.x;
-				imageObject.height += loc.y - dragLoc.y;
-				dragLoc.x = loc.x;
-				dragLoc.y = loc.y;
-			}
-			else
-			{
-				var s = calculateAspectRatioFit(imageObject.width, imageObject.height, imageObject.width += loc.x - dragLoc.x, imageObject.height += loc.y - dragLoc.y)
-				imageObject.width = s.width;
-				imageObject.height = s.height;
-
-				dragLoc.x = imageObject.x + imageObject.width;
-				dragLoc.y = imageObject.y + imageObject.height;
-			}
-			renderAll();
-		}
-		else
-		{
-			if (clickObject == true) {
-				if (hitTest(loc, selectObjectGrabHandle.x1, selectObjectGrabHandle.y1, selectObjectGrabHandle.x2 - selectObjectGrabHandle.x1, selectObjectGrabHandle.y2 - selectObjectGrabHandle.y1)) {
-					canvas.style.cursor = "nw-resize";
-				}
-				else
-				{
-					canvas.style.cursor = "default";
-				}
-			}
-			else
-			{
-				canvas.style.cursor = "default";
-			}
-		}
-	}
-
-	function hitTest(loc, x, y, width, height) {
-		context.beginPath();
-		context.rect(x, y, width, height);
-
-		if (context.isPointInPath(loc.x, loc.y)) {
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	function windowToCanvas(x, y) {
-		var bbox = canvas.getBoundingClientRect();
-		return { x : x - bbox.left * (canvas.width / bbox.width),
-				 y : y - bbox.top  * (canvas.height / bbox.height) };
-	}
-
-	function renderGrid(color, stepx, stepy) {
-		context.save()
-
-		context.strokeStyle = color;
-		context.lineWidth = 0.5;
-		context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-
-		for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
-			context.beginPath();
-			context.moveTo(i, 0);
-			context.lineTo(i, context.canvas.height);
-			context.stroke();
-		}
-
-		for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
-			context.beginPath();
-			context.moveTo(0, i);
-			context.lineTo(context.canvas.width, i);
-			context.stroke();
-		}
-		context.restore();
-	}
-
-	function renderObject() {
-		//context.save();
-		context.globalAlpha = 1;
-		context.drawImage(image, Math.floor(imageObject.x), Math.floor(imageObject.y), Math.floor(imageObject.width), Math.floor(imageObject.height) );
-		//context.restore();
-	}
-
-	function selectObject() {
-		context.save();
-
-		calculateObjectSizes();
-		context.beginPath();
-
-		dashedLinePath(context, selectObjectBorder.x1, selectObjectBorder.y1, selectObjectBorder.x2, selectObjectBorder.y1, 4);
-		dashedLinePath(context, selectObjectBorder.x2, selectObjectBorder.y1, selectObjectBorder.x2, selectObjectBorder.y2, 4);
-		dashedLinePath(context, selectObjectBorder.x2, selectObjectBorder.y2, selectObjectBorder.x1, selectObjectBorder.y2, 4);
-		dashedLinePath(context, selectObjectBorder.x1, selectObjectBorder.y2, selectObjectBorder.x1, selectObjectBorder.y1, 4);
-
-		context.stroke();
-
-		context.fillStyle = "white";
-		context.fillRect(selectObjectGrabHandle.x1, selectObjectGrabHandle.y1, selectObjectGrabHandle.x2 - selectObjectGrabHandle.x1, selectObjectGrabHandle.y2 - selectObjectGrabHandle.y1);
-
-		context.strokeStyle = "gray";
-		context.strokeRect(selectObjectGrabHandle.x1, selectObjectGrabHandle.y1, selectObjectGrabHandle.x2 - selectObjectGrabHandle.x1, selectObjectGrabHandle.y2 - selectObjectGrabHandle.y1);
-
-		context.restore();
-	}
-
-	function calculateObjectSizes() {
-		selectObjectBorder = {	x1 : Math.floor(imageObject.x) - selectObjectBorderPadding - 0.5,
-								y1 : Math.floor(imageObject.y) - selectObjectBorderPadding - 0.5,
-								x2 : Math.floor(imageObject.x) + Math.floor(imageObject.width) + selectObjectBorderPadding + 0.5,
-								y2 : Math.floor(imageObject.y) + Math.floor(imageObject.height) + selectObjectBorderPadding + 0.5};
-
-		selectObjectGrabHandle = {	x1 : Math.floor(imageObject.x) + Math.floor(imageObject.width) - (selectObjectGrabWidth / 2) - 0.5,
-									y1 : Math.floor(imageObject.y) + Math.floor(imageObject.height) - (selectObjectGrabWidth / 2) - 0.5,
-									x2 : Math.floor(imageObject.x) + Math.floor(imageObject.width) + (selectObjectGrabWidth / 2) + 0.5,
-									y2 : Math.floor(imageObject.y) + Math.floor(imageObject.height) + (selectObjectGrabWidth / 2) + 0.5 };
-	}
-
-	function dashedLinePath(context, x1, y1, x2, y2, dashLength) {
-		dashLength = dashLength === undefined ? 5 : dashLength;
-
-		var deltaX = x2 - x1;
-		var deltaY = y2 - y1;
-		var numDashes = Math.floor(
-			Math.sqrt(deltaX * deltaX + deltaY * deltaY) / dashLength);
-
-		for (var i=0; i < numDashes; ++i) {
-			context[ i % 2 === 0 ? 'moveTo' : 'lineTo' ]
-				(x1 + (deltaX / numDashes) * i, y1 + (deltaY / numDashes) * i);
-		}
-	}
-
-	function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
-		var ratio = [maxWidth / srcWidth, maxHeight / srcHeight ];
-		ratio = Math.min(ratio[0], ratio[1]);
-
-		return { width:srcWidth*ratio, height:srcHeight*ratio };
-	}
-
-	function renderAll() {
-		restoreCanvasBackground();
-		renderObject();
-
-		if (clickObject == true) {
-			//context.fillRect(10, 10, 10, 10);
-			selectObject();
-		}
-	}
-
-	function saveCanvasBackground() {
-		canvasBackgroundImageData = context.getImageData(0, 0, canvas.width, canvas.height);
-	}
-
-	function restoreCanvasBackground() {
-		context.putImageData(canvasBackgroundImageData, 0, 0);
-	}
-
-	function initialise_canvas() {
-				renderGrid('lightgray', 10, 10);
-				saveCanvasBackground();
-				renderAll();
-    }
-
-*/
